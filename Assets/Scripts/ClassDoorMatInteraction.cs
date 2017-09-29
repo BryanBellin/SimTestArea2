011328194606456cf4f2786880f5d93c47d8168a
BryanBellin <bryanbellin@gmail.com>
2017-09-29 10:59:05 -0700

    Added Continue Reminder
    Black Screen now has reminder to press any key to continue

--- Assets/Scripts/ClassDoorMatInteraction.cs
+++ Assets/Scripts/ClassDoorMatInteraction.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class ClassDoorMatInteraction : MonoBehaviour {
 
     public Transform Player;
-    public GameObject ActionDisplay;     public GameObject ActionText;     public GameObject Background;     public GameObject ResultText;       private static int Prompt = 0;     private static int Continue = 0;      private void OnCollisionEnter(Collision collision)     {         if(collision.gameObject.CompareTag("Player"))         {             ActionDisplay.SetActive(true);             ActionText.SetActive(true);             Prompt = 1;         }     }      private void OnCollisionExit(Collision collision)     {         if (collision.gameObject.CompareTag("Player"))         {             ActionDisplay.SetActive(false);             ActionText.SetActive(false);             Prompt = 0;         }     }      private void Update()
+    public GameObject ActionDisplay;     public GameObject ActionText;     public GameObject Background;     public GameObject ResultText;
+    public GameObject ContinueReminder;      private static int Prompt = 0;     private static int Continue = 0;      private void OnCollisionEnter(Collision collision)     {         if(collision.gameObject.CompareTag("Player"))         {             ActionDisplay.SetActive(true);             ActionText.SetActive(true);             Prompt = 1;         }     }      private void OnCollisionExit(Collision collision)     {         if (collision.gameObject.CompareTag("Player"))         {             ActionDisplay.SetActive(false);             ActionText.SetActive(false);             Prompt = 0;         }     }      private void Update()
     {
         if(Prompt == 1)
         {
@@ -11,11 +12,12 @@
                 ActionText.SetActive(false);
                 Background.SetActive(true);
                 ResultText.SetActive(true);
+                ContinueReminder.SetActive(true);
 
-                
-                
+
+
             }
-            if(Input.GetButtonUp ("Interact"))
+            if (Input.GetButtonUp ("Interact"))
             {
                 Continue = 1;
             }
@@ -25,6 +27,7 @@
                 {
                     Background.SetActive(false);
                     ResultText.SetActive(false);
+                    ContinueReminder.SetActive(false);
                     Player.position = new Vector3 (0, 0, 0);
                     Continue = 0;
                     Prompt = 0;
