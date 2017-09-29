a42a3c0537bc60b9941f4bba62092e46ce5333ae
BryanBellin <bryanbellin@gmail.com>
2017-09-29 14:15:04 -0700

    Added Random Stat Increase

--- Assets/Scripts/ClassDoorMatInteraction.cs
+++ Assets/Scripts/ClassDoorMatInteraction.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class ClassDoorMatInteraction : MonoBehaviour {
+﻿using System.Collections; using System.Collections.Generic; using UnityEngine; using UnityEngine.UI;  public class ClassDoorMatInteraction : MonoBehaviour {
 
     public Transform Player;
     public GameObject ActionDisplay;     public GameObject ActionText;     public GameObject Background;     public GameObject ResultText;
-    public GameObject ContinueReminder;      private static int Prompt = 0;     private static int Continue = 0;      private void OnCollisionEnter(Collision collision)     {         if(collision.gameObject.CompareTag("Player"))         {             ActionDisplay.SetActive(true);             ActionText.SetActive(true);             Prompt = 1;         }     }      private void OnCollisionExit(Collision collision)     {         if (collision.gameObject.CompareTag("Player"))         {             ActionDisplay.SetActive(false);             ActionText.SetActive(false);             Prompt = 0;         }     }      private void Update()
+    public GameObject ContinueReminder;     public Text MajorProficiencyText;     public int MajorProficiencyValue;      private static int Prompt = 0;     private static int Continue = 0;      private void OnCollisionEnter(Collision collision)     {         if(collision.gameObject.CompareTag("Player"))         {             ActionDisplay.SetActive(true);             ActionText.SetActive(true);             Prompt = 1;         }     }      private void OnCollisionExit(Collision collision)     {         if (collision.gameObject.CompareTag("Player"))         {             ActionDisplay.SetActive(false);             ActionText.SetActive(false);             Prompt = 0;         }     }      private void Update()
     {
         if(Prompt == 1)
         {
@@ -13,9 +13,7 @@
                 Background.SetActive(true);
                 ResultText.SetActive(true);
                 ContinueReminder.SetActive(true);
-
-
-
+                
             }
             if (Input.GetButtonUp ("Interact"))
             {
@@ -29,10 +27,16 @@
                     ResultText.SetActive(false);
                     ContinueReminder.SetActive(false);
                     Player.position = new Vector3 (0, 0, 0);
+                    MajorProficiencyValue = MajorProficiencyValue + Mathf.RoundToInt(Random.Range(1,5));
+                    UpdateMajorProficiency();
                     Continue = 0;
                     Prompt = 0;
                 }
             }
             
         }
+    }      void UpdateMajorProficiency()
+    {
+        
+        MajorProficiencyText.text = "Major Proficiency: " + MajorProficiencyValue.ToString() + "/12320";
     } } 
