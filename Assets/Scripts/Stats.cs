c4e2c20828eed9ecb775a0211cde2589565b7da3
BryanBellin <bryanbellin@gmail.com>
2017-09-30 21:51:42 -0700

    Added IRIS
    Parent Child relationship established.  Now stats are stored in IRIS and get read and written when messaged

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stats : MonoBehaviour {
+
+    public Text MajorProficiencyText;
+    public int MajorProficiencyValue;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void UpdateMajorProficiency(int value)
+    {
+        MajorProficiencyValue = MajorProficiencyValue + value;
+        MajorProficiencyText.text = "Major Proficiency: " + MajorProficiencyValue.ToString() + "/12320";
+    }
+}
